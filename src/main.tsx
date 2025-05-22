d6393bf382fb4c204d7996f070db7f63e6fad154
adiiwill <gazdagadam02@gmail.com>
2025-05-22 13:58:37 +0200

    Removed svgs and comments

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
 
-// import { HeroUIProvider } from '@heroui/system'
-
 createRoot(document.querySelector('body')!).render(
   <StrictMode>
     <App />
