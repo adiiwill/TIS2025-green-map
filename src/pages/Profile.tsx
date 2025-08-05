89f725211506c7e372312ba5beb52d0f734749df
Gazdag Ádám <gazdagadam02@gmail.com>
2025-08-05 16:21:57 +0200

    TA25-7: Profile page bottom margin fix

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -15,7 +15,7 @@ const Profile = () => {
 
   return (
     <Layout title={t('profile.title')}>
-      <div className="bg-fgWhite dark:bg-bgDark dark:text-white min-h-screen lg:min-h-[calc(100vh-80px)] p-4 sm:p-8 md:p-12 lg:p-16 pb-20 sm:pb-28 lg:pb-36">
+      <div className="bg-fgWhite dark:bg-bgDark dark:text-white min-h-screen lg:min-h-[calc(100vh-80px)] p-4 sm:p-8 sm:pb-32 md:p-24 md:pb-32 lg:p-16 pb-20 lg:pb-36">
         <div className="flex gap-6 sm:gap-8 md:gap-12 lg:gap-28 mb-8 sm:mb-12 md:mb-16 lg:mb-18 flex-col md:flex-row items-center">
           <div className="h-24 w-24 sm:h-32 sm:w-32 md:w-40 md:h-40 lg:w-52 lg:h-52 aspect-square bg-[#d9d9d9] rounded-full flex items-center justify-center border-[#3F3D56] border-2">
             <span className="font-merryweather text-black text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-bold">
