033f00fb0d812fa8732984aa259ff85c7dbcdd2e
Gazdag Ádám <gazdagadam02@gmail.com>
2025-08-06 11:50:14 +0200

    TA25-7: Admin page overflow problem fix

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -108,7 +108,7 @@ const Admin = () => {
   return (
     <Layout title={t('admin.title')} extended>
       <div className="bg-[#f2f2f2] dark:bg-bgDark min-h-screen flex flex-col">
-        <div className="grid grid-cols-[repeat(auto-fit,375px)] justify-center gap-6 overflow-auto p-4 lg:p-14 pt-[200px] pb-6">
+        <div className="grid grid-cols-[repeat(auto-fit,375px)] justify-center gap-6 overflow-auto overflow-x-hidden p-4 lg:p-14 pt-[200px] pb-6">
           {displayPois.map((poi) => (
             <PoiItemTile key={poi.id} item={poi} />
           ))}
